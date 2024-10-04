d3c2dc0bd7b3b06383d90ac6825320a33888a10f
Ayoub Gouasmi <96791563+gougouasmi@users.noreply.github.com>
2024-10-04 11:10:52 -0700

    Added output factors to compare_edges program

--- python/compare_edges.py
+++ python/compare_edges.py
@@ -59,6 +59,31 @@ def compare_edge_conditions(
     c2_grid_0 = 2. * xi_grid_0[1:] * dhe_dxi_grid_0[1:] / he_grid_0[1:]
     eck_0 = ue_grid_0**2 / he_grid_0
 
+    grid_0_size = x_grid_0.shape[0]
+    output_factors_0 = {
+        'tau': np.zeros(grid_0_size), #,
+        'q': np.zeros(grid_0_size), #,
+        'y': np.zeros(grid_0_size), #,
+    }
+
+    dxi_dx_grid_0 = edge_grid_0[edge_indices_0["dxi/dx"], :]
+
+    romu_stag_0 = (
+        edge_grid_0[edge_indices_0["roe"], 0] *
+        edge_grid_0[edge_indices_0["mue"], 0]
+    )
+ 
+    he_stag_0 = he_grid_0[0]
+    due_dx_stag_0 = (ue_grid_0[1] - ue_grid_0[0]) / (x_grid_0[1] - x_grid_0[0])
+
+    output_factors_0["tau"][0] = (romu_stag_0 ** 0.5) * (due_dx_stag_0 ** 1.5) * x_grid_0[0]
+    output_factors_0["q"][0]   = he_stag_0 * (romu_stag_0 * due_dx_stag_0) ** 0.5
+    output_factors_0["y"][0]   = (romu_stag_0 / due_dx_stag_0) ** 0.5
+
+    output_factors_0["tau"][1:] = ue_grid_0[1:] * dxi_dx_grid_0[1:] / np.sqrt(2. * xi_grid_0[1:])
+    output_factors_0["q"][1:]   = he_grid_0[1:] * dxi_dx_grid_0[1:] / np.sqrt(2. * xi_grid_0[1:])
+    output_factors_0["y"][1:]   = np.sqrt(2. * xi_grid_0[1:]) / ue_grid_0[1:]
+
     # Second dataset
     ue_grid_1 = edge_grid_1[edge_indices_1["ue"], :]
     he_grid_1 = edge_grid_1[edge_indices_1["he"], :]
@@ -73,6 +98,31 @@ def compare_edge_conditions(
     c2_grid_1 = 2. * xi_grid_1[1:] * dhe_dxi_grid_1[1:] / he_grid_1[1:]
     eck_1 = ue_grid_1**2 / he_grid_1
 
+    grid_1_size = x_grid_1.shape[0]
+    output_factors_1 = {
+        'tau': np.zeros(grid_1_size), #,
+        'q': np.zeros(grid_1_size), #,
+        'y': np.zeros(grid_1_size), #,
+    }
+
+    dxi_dx_grid_1 = edge_grid_1[edge_indices_1["dxi/dx"], :]
+
+    romu_stag_1 = (
+        edge_grid_1[edge_indices_1["roe"], 0] *
+        edge_grid_1[edge_indices_1["mue"], 0]
+    )
+ 
+    he_stag_1 = he_grid_1[0]
+    due_dx_stag_1 = (ue_grid_1[1] - ue_grid_1[0]) / (x_grid_1[1] - x_grid_1[0])
+
+    output_factors_1["tau"][0] = (romu_stag_1 ** 0.5) * (due_dx_stag_1 ** 1.5) * x_grid_1[0]
+    output_factors_1["q"][0]   = he_stag_1 * (romu_stag_1 * due_dx_stag_1) ** 0.5
+    output_factors_1["y"][0]   = (romu_stag_1 / due_dx_stag_1) ** 0.5
+
+    output_factors_1["tau"][1:] = ue_grid_1[1:] * dxi_dx_grid_1[1:] / np.sqrt(2. * xi_grid_1[1:])
+    output_factors_1["q"][1:]   = he_grid_1[1:] * dxi_dx_grid_1[1:] / np.sqrt(2. * xi_grid_1[1:])
+    output_factors_1["y"][1:]   = np.sqrt(2. * xi_grid_1[1:]) / ue_grid_1[1:]
+
     # Bounds
     x1  = max(np.max(x_grid_0),  np.max(x_grid_1)) 
     xi1 = max(np.max(xi_grid_0), np.max(xi_grid_1))
@@ -80,7 +130,10 @@ def compare_edge_conditions(
     # Figure parameters
     vert_space = 5
 
-    ## Flow conditions in physical space
+    ###
+    # Flow conditions in physical space
+    #
+
     fig0, axes = plt.subplots(nrows=1, ncols=3, figsize=(14, vert_space))
     
     fig0.suptitle("Flow conditions (physical space)")
@@ -105,7 +158,10 @@ def compare_edge_conditions(
         ax.set_xlim([0, x1])
         ax.set_xlabel(r"$x$")
 
-    ## Flow conditions in reference space  
+    ###
+    # Flow conditions in reference space  
+    #
+        
     fig1, axes = plt.subplots(nrows=1, ncols=3, figsize=(14, vert_space))
     
     fig1.suptitle(r"Flow conditions ($\xi$-space)")
@@ -130,9 +186,10 @@ def compare_edge_conditions(
         ax.set_xlim([0, xi1])
         ax.set_xlabel(r"$\xi$")
 
-    ## Local-Similarity Coefficients
-
+    ###
+    # Local-Similarity Coefficients
     #
+        
     fig2, axes = plt.subplots(nrows=1, ncols=3, figsize=(14, vert_space))
 
     fig2.suptitle("Local-Similarity coefficients")
@@ -159,7 +216,39 @@ def compare_edge_conditions(
         ax.grid(which="both")
         ax.set_xlim([0, xi1])
         ax.set_xlabel(r"$\xi$")
-    
+
+    ###
+    # Output factors
+    #
+
+    fig3, axes = plt.subplots(nrows=1, ncols=3, figsize=(14, vert_space))
+
+    fig3.suptitle("Output factors")
+
+    ax1, ax2, ax3 = axes
+
+    ax1.plot(x_grid_0, output_factors_0["tau"], marker='x', label=label_0)
+    ax1.plot(x_grid_1, output_factors_1["tau"], marker='x', label=label_1)
+
+    ax1.set_title(r"$\tau$")
+
+    ax2.plot(x_grid_0, output_factors_0["q"], marker='x', label=label_0)
+    ax2.plot(x_grid_1, output_factors_1["q"], marker='x', label=label_1)
+
+    ax2.set_title(r"$q$")
+
+    ax3.plot(x_grid_0, output_factors_0["y"], marker='x', label=label_0)
+    ax3.plot(x_grid_1, output_factors_1["y"], marker='x', label=label_1)
+
+    ax3.set_title(r"$y$")
+
+    for ax in axes:
+        ax.legend()
+        ax.grid(which="both")
+        ax.set_xlim([0, x1])
+        ax.set_xlabel(r"$x$")
+
+
     plt.show()
  
 ###
